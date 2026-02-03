24f12dd725b7416250afc6285fc9e55dba858076
Frostnout <dima.burbas@gmail.com>
2026-02-03 20:51:49 +0300

    feat: add del button for img in create-ad

--- frontend/assets/js/create-ad.js
+++ frontend/assets/js/create-ad.js
@@ -2,17 +2,35 @@ const imageInput = document.getElementById('imageInput');
 const preview = document.getElementById('imagePreview');
 
 imageInput.addEventListener('change', () => {
-  preview.innerHTML = '';
-
   Array.from(imageInput.files).forEach(file => {
+    if (!file.type.startsWith('image/')){ return; }
+
     const reader = new FileReader();
 
     reader.onload = e => {
+      const wrapper = document.createElement('div');
+      wrapper.className = 'image-item';
+
       const img = document.createElement('img');
       img.src = e.target.result;
-      preview.appendChild(img);
+
+      const btn = document.createElement('button');
+      btn.className = 'remove-btn';
+      btn.innerHTML = '&times;';
+      btn.type = "button"; 
+
+      btn.onclick = () => {
+        wrapper.remove();
+      };
+
+      wrapper.appendChild(img);
+      wrapper.appendChild(btn);
+      preview.appendChild(wrapper);
     };
 
     reader.readAsDataURL(file);
   });
+  
+  imageInput.value = ''; 
 });
+
