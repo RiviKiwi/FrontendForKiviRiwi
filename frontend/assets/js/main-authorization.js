a33bd07b3225b6e1710daa290a5e7ff78b0804e3
Frostnout <dima.burbas@gmail.com>
2026-02-04 15:07:08 +0300

    refactor: icons for authorization

--- frontend/assets/js/main-authorization.js
+++ frontend/assets/js/main-authorization.js
@@ -1,20 +1,27 @@
 const passwordFields = document.querySelectorAll('.password-field');
 
 passwordFields.forEach(field => {
-  const passwordInput = field.querySelector('input');
-  const eyeIcon = field.querySelector('.eye img');
-  const avatarIcon = document.querySelector('.avatar img');
-  if (passwordInput && eyeIcon) {
-    eyeIcon.addEventListener('click', () => {
-      const isHidden = passwordInput.type === 'password';
-      
-      passwordInput.type = isHidden ? 'text' : 'password';
-      eyeIcon.src = isHidden
-        ? 'assets/images/icons/opened-eye.svg'
-        : 'assets/images/icons/closed-eye.svg';
-      avatarIcon.src = isHidden
-        ? 'assets/images/icons/bird-open-svg.svg'
-        : 'assets/images/icons/bird-log.svg';
-    });
-  }
+    const passwordInput = field.querySelector('input');
+
+    const eyeUse = field.querySelector('.eye svg use'); 
+
+    const avatarIcon = document.querySelector('.avatar img'); 
+
+    if (passwordInput && eyeUse) {
+        field.querySelector('.eye').addEventListener('click', () => {
+            const isHidden = passwordInput.type === 'password';
+            passwordInput.type = isHidden ? 'text' : 'password';
+
+            const newIconId = isHidden 
+                ? 'assets/images/icons/icons.svg#open-eye-icon' 
+                : 'assets/images/icons/icons.svg#closed-eye-icon';
+            
+            eyeUse.setAttribute('href', newIconId);
+            if (avatarIcon) {
+                avatarIcon.src = isHidden 
+                    ? 'assets/images/icons/bird-open-svg.svg' 
+                    : 'assets/images/icons/bird-log.svg';
+            }
+        });
+    }
 });
